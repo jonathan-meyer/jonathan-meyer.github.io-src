eab1591839e9f3aed6720e2c285350681784c9dd
JONATHAN MEYER <JONATHAN.MEYER@LIBERTYMUTUAL.COM>
2019-04-25 11:59:38 -0400

    bootstrap...

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <pre>As you walk down the long dark tunnel your sword starts to glow a faint blue.</pre>
-        <a className="App-link" href="/old" rel="noopener noreferrer">
-          continue...
-        </a>
+        <Container>
+          <Row>
+            <Col>As you walk down the long dark tunnel your sword starts to glow a faint blue.</Col>
+          </Row>
+          <Row>
+            <Col>
+              <Button variant="info" href="/old">
+                continue...
+              </Button>
+            </Col>
+          </Row>
+        </Container>
       </header>
     </div>
   );
