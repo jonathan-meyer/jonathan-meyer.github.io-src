cea2b582ca053622f2a60078d19b44367e4c56e7
JONATHAN MEYER <JONATHAN.MEYER@LIBERTYMUTUAL.COM>
2019-04-25 10:37:57 -0400

    added link to old site

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat rerum ipsam numquam blanditiis nisi eaque, unde tempora error. Repellat nobis
-          maiores enim consequatur omnis praesentium quibusdam dolore unde? Reiciendis, accusamus?
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
+        <pre>As you walk down the long dark tunnel your sword starts to glow a faint blue.</pre>
+        <a className="App-link" href="/old" rel="noopener noreferrer">
+          continue...
         </a>
       </header>
     </div>
