34df35d83513e00f4817559e96c4911e58ef1db3
JONATHAN MEYER <JONATHAN.MEYER@LIBERTYMUTUAL.COM>
2019-05-28 13:45:23 -0400

    ...

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 import Image from "react-bootstrap/Image";
-import Timeline from "react-dual-timeline";
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 
-function About() {
+import "react-vertical-timeline-component/style.min.css";
+
+const data = [["1974", <h2>Born</h2>], ["1992", <h2>High School</h2>]];
+
+const About = () => {
   return (
     <Container className="my-3">
       <Card className="bg-dark text-light">
@@ -15,13 +19,17 @@ function About() {
           <Card.Title>White and Nerdy</Card.Title>
           <Image className="float-left mr-3" thumbnail={false} rounded={true} src="../old/mug.jpg" />
           <Card.Text>I'm an average guy who likes nerdy things.</Card.Text>
-          <Timeline>
-            <div icon="x">Born</div>
-          </Timeline>
         </Card.Body>
       </Card>
+      <VerticalTimeline>
+        {data.map((e, k) => (
+          <VerticalTimelineElement iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }} key={k} date={e[0]}>
+            {e[1]}
+          </VerticalTimelineElement>
+        ))}
+      </VerticalTimeline>
     </Container>
   );
-}
+};
 
 export default About;
